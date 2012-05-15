c33efb9c7ecefef49d77944ed6a1daffa5746acd
qk168899 <qk168899@gmail.com>
2012-05-15 09:49:11 +0000

    feature ZSS-124: Support pivot table

---
+++ src/java/org/apache/poi/ss/util/ItemInfo.java
@@ -0,0 +1,66 @@
+/* ItemInfo.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		May 15, 2012 9:51:32 AM , Created by Sam
+}}IS_NOTE
+
+Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.poi.ss.util;
+
+import org.zkoss.poi.ss.usermodel.PivotField.Item;
+import org.zkoss.poi.ss.usermodel.PivotField.Item.Type;
+
+/**
+ * @author Sam
+ *
+ */
+public class ItemInfo {
+
+	private Object _value;
+	private int _depth;
+	private Item.Type _type;
+	
+	public ItemInfo(Type type, Object value, int depth) {
+		_type = type;
+		_value = value;
+		_depth = depth;
+	}
+	
+	/**
+	 * Returns the type.
+	 * 
+	 * @return
+	 */
+	public Type getType () {
+		return _type;
+	}
+
+	/**
+	 * Returns the value of item.
+	 * 
+	 * @return the value
+	 */
+	public Object getValue() {
+		return _value;
+	}
+
+	/**
+	 * Returns the depth of the item.
+	 * 
+	 * @return the depth
+	 */
+	public int getDepth() {
+		return _depth;
+	}
+	
+	
+}
